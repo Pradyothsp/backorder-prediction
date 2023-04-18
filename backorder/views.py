9d1b629de2d0d5f15234c48ba761e9f1da59bb34
Pradyoth S P <pradyothsp@Pradyoths-MacBook-Pro.local>
2023-04-17 20:29:14 -0400

    Front-end individual form working

--- backorder/views.py
+++ backorder/views.py
@@ -5,13 +5,14 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from backorder.pipeline import BackorderPredictor
+from backorder.utils import get_form_data
 
 backorder_predictor = BackorderPredictor()
 backorder_predictor._load_model_files()
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'backorder.html')
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -22,13 +23,36 @@ class PredictView(View):
         return render(request, 'predict.html')
 
     def post(self, request, *args, **kwargs):
-        file = request.FILES['file']
-        df = pd.read_csv(file)
+        file = self.request.FILES.get('file')
+        # print(request.POST)
+        if file:
+            file = request.FILES['file']
+            df = pd.read_csv(file)
+            print(df.to_dict('dict'))
+            y_pred, y_actual = backorder_predictor.predict(df)
 
-        y_pred, y_actual = backorder_predictor.predict(df)
+            context = {
+                'y_pred': y_pred,
+                'y_actual': y_actual,
+            }
+            return render(request, 'result.html', context)
 
-        context = {
-            'y_pred': y_pred,
-            'y_actual': y_actual,
-        }
-        return render(request, 'result.html', context)
+        elif all(key in request.POST for key in
+                 ['sku', 'national_inv', 'lead_time', 'in_transit_qty', 'forecast_3_month', 'forecast_6_month',
+                  'forecast_9_month', 'sales_1_month', 'sales_3_month', 'sales_6_month', 'sales_9_month', 'min_bank',
+                  'pieces_past_due', 'perf_6_month_avg', 'perf_12_month_avg', 'local_bo_qty']):
+            form_data = get_form_data(request)
+            df = pd.DataFrame(form_data)
+            print(df.to_dict('dict'))
+
+
+            y_pred, y_actual = backorder_predictor.predict(df)
+
+            context = {
+                'y_pred': y_pred,
+                'y_actual': y_actual,
+            }
+            return render(request, 'result.html', context)
+
+        else:
+            return render(request, 'predict.html')
